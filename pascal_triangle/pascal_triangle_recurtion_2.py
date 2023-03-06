58ba78f1f3bf286a65d705339e1561b1ebd06393
samitha <122595628+samitha278@users.noreply.github.com>
2023-03-06 17:25:47 +0530

    Update pascal_triangle_recurtion_2.py
    previous code have logical error

--- pascal_triangle/pascal_triangle_recurtion_2.py
+++ pascal_triangle/pascal_triangle_recurtion_2.py
@@ -1,17 +1,21 @@
 class Pascal(object): 
+        
     def genarate_pascal(self,numRows):
         if(numRows==1):
             return [[1]]
         
+        
         pascal = self.genarate_pascal(numRows-1)
             
         i = 1
+        j = numRows-2
         row = [1]
+        
         while(i<numRows):
             if i == numRows-1:
                 row.append(1)
             else:
-                row.append(pascal[i][i-1]+pascal[i][i])
+                row.append(pascal[j][i-1]+pascal[j][i])
                 
             i+=1
                 
