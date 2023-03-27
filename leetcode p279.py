e3d21a838173c1279e75c0ac6126f4ed6cc70669
Samitha Bandara <122595628+samitha278@users.noreply.github.com>
2023-03-27 15:54:40 +0530

    Create leetcode p279.py

---
+++ leetcode p279.py
@@ -0,0 +1,39 @@
+class Solution: 
+
+    def numSquares(self,n:int) -> int:
+
+        SqrList = [i**2 for i in range(1,int((n**0.5)+1))]
+        SqrList.sort(reverse=True)
+        numSqrList = []
+
+        for j in range(len(SqrList)):
+            if SqrList[j]==1:
+                break
+            x = n - SqrList[j]
+            numPerSqr = 1
+
+            while x != 0 :
+                if x in SqrList:
+                    numPerSqr+=1
+                    break
+                else:
+                    if(x < 4):
+                        numPerSqr+=x
+                        break
+                    else:
+                        nSqrlist = [i**2 for i in range(1,int((x**0.5)+1))]
+                        nSqrlist.sort(reverse=True)
+                        x = x - nSqrlist[0]
+                        numPerSqr +=1
+                        
+            numSqrList.append(numPerSqr)
+
+        return min(numSqrList)
+
+def main():
+    x = Solution()
+    y = x.numSquares(int(input("Enter: ")))
+    print(y)
+
+if __name__ == '__main__':
+    main()
